1966267f9dba8b5c91d0da83495c85e1aed929ff
Vinicius-Felix <marcus.felix>
2019-06-20 12:16:48 -0300

    Tela principal

--- FinancialSystem/FinancialSystem/FormMenu.cs
+++ FinancialSystem/FinancialSystem/FormMenu.cs
@@ -10,11 +10,66 @@ using System.Windows.Forms;
 
 namespace FinancialSystem
 {
-    public partial class Form1 : Form
+    public partial class FormMenu : Form
     {
-        public Form1()
+        private Button btnFechar;
+        private Panel panelCabecalho;
+
+        public FormMenu()
         {
             InitializeComponent();
+        }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelCabecalho = new System.Windows.Forms.Panel();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.panelCabecalho.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelCabecalho
+            // 
+            this.panelCabecalho.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panelCabecalho.Controls.Add(this.btnFechar);
+            this.panelCabecalho.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelCabecalho.Location = new System.Drawing.Point(0, 0);
+            this.panelCabecalho.Name = "panelCabecalho";
+            this.panelCabecalho.Size = new System.Drawing.Size(284, 100);
+            this.panelCabecalho.TabIndex = 0;
+            this.panelCabecalho.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFechar.FlatAppearance.BorderSize = 0;
+            this.btnFechar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnFechar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.Location = new System.Drawing.Point(0, 0);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 0;
+            this.btnFechar.Text = "button1";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            // 
+            // FormMenu
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panelCabecalho);
+            this.Name = "FormMenu";
+            this.panelCabecalho.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
