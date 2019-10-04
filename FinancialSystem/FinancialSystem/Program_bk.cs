e42dbf600b06ff15e1497d32291ac49e944179d7
Adriano Laureano <adriano.laureano@seredesa.com.br>
2019-10-04 14:04:12 -0300

    [ADD] add program bk

---
+++ FinancialSystem/FinancialSystem/Program_bk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinancialSystem
+{
+    static class Program_bk
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main_bk()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMenu());
+        }
+    }
+}
